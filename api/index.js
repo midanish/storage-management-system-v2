1925618f7b1c29887f468ca6ef95670d0a66f6d1
balqisnaguwib <balqisnaguwib@gmail.com>
2025-09-29 01:14:32 +0800

    refactor: enhance Vercel compatibility by updating API handler and configuration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
+const express = require('express');
 const app = require('../server.js');
 
-// Export a handler compatible with Vercel serverless functions (updated)
+// Export a handler compatible with Vercel serverless functions
 module.exports = async (req, res) => {
-  // Forward the request to the Express app
-  return app(req, res);
+  return new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
 };
